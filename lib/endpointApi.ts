9abab348b1a546d20369c7da59f57e2fde3d18cb
juniordev203 <ttk11092k3@gmail.com>
2025-07-11 11:01:59 +0700

    feat: config axios & update endpointApi

--- lib/endpointApi.ts
+++ lib/endpointApi.ts
@@ -4,6 +4,33 @@ const ApiUrl = {
     login: "/auth/login",
     logout: "/auth/logout",
   },
+  user: {
+    profile: "/user/info-user",
+    updateProfile: "/user/update/{userId}",
+  },
+  flashcard: {
+    foldersByUserId: "/api/Flashcard/flashcard-folders/{userId}",
+    setsByFolderId: "/api/Flashcard/flashcard-sets/folder/{folderId}",
+    setsByUserId: "/api/Flashcard/flashcard-sets/user/{userId}",
+    setDetails: "/api/Flashcard/flashcard-set/details/{setId}",
+    setById: "/api/Flashcard/flashcard/set/{setId}",
+    favoritesByUserId: "/api/Flashcard/flashcard/favorites/{userId}",
+    gameResultByUser: "/api/Flashcard/flashcard/game-result/user/{userId}",
+    gameResultBySet: "/api/Flashcard/flashcard/game-result/set/{setId}",
+
+    createFolder: "/api/Flashcard/flashcard-folder",
+    createSet: "/api/Flashcard/flashcard-set",
+    createFlashcard: "/api/Flashcard/flashcard",
+    createGameResult: "/api/Flashcard/flashcard/game/result",
+
+    toggleFavorite: "/api/Flashcard/flashcard/{id}/favorite",
+    updateFolderName: "/api/Flashcard/flashcard/folder-name/{id}",
+    updateSetName: "/api/Flashcard/flashcard/set-name/{id}",
+
+    deleteFolder: "/api/Flashcard/flashcard-folder-delete/{id}",
+    deleteSet: "/api/Flashcard/flashcard-set-delete/{id}",
+    deleteFlashcard: "/api/Flashcard/flashcard-delete/{id}",
+  },
 };
 
 export default ApiUrl;
